a5b85c81c6af8c473add44c287e5a8d0327e90ef
Himanshu Sharma <himanshusharma4all@outlook.com>
2025-10-23 11:28:19 +0530

    indentation

--- src/app/core/guards/auth-guard-guard.ts
+++ src/app/core/guards/auth-guard-guard.ts
@@ -3,10 +3,13 @@ import { CanActivateFn, Router } from '@angular/router';
 import { Session } from '../services/session';
 
 export const authGuardGuard: CanActivateFn = (route, state) => {
+  
   const session = inject(Session);
   const router = inject(Router);
+
   const user_token = session.getSessionStorage('auth_token');
   const userDetails = session.getSessionStorageObject<any>('details');
+
   if(!user_token || !userDetails) {
     console.log('User is not authenticated, redirecting to login.');
     router.navigateByUrl('/login');
